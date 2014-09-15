ce448832af1007098798a270e19b1199df1660e7
Adam Weiner <adamwineguy@gmail.com>
2014-09-15 11:40:46 -0600

    javasctipt name exercise

--- main.js
+++ main.js
@@ -1,1 +1,4 @@
-alert('Hello Boulder!');
+//alert('Hello Boulder!');
+var first = prompt("Please enter your first name");
+var last = prompt("Please enter your last name")
+alert("Hello" + first + " " + last + "!")
