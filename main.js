30e4de0a87df2673c8c5e87cf84c4787ae3e13f3
Adam Weiner <adamwineguy@gmail.com>
2014-09-16 14:03:25 -0600

    cleaned up code

--- main.js
+++ main.js
@@ -1,70 +1,70 @@
-//alert('Hello Boulder!');
+alert('Hello Boulder!');
 
 
-//var first = prompt("Please enter your first name");
-//var last = prompt("Please enter your last name")
-//alert("Hello" + first + " " + last + "!")
+var first = prompt("Please enter your first name");
+var last = prompt("Please enter your last name");
+alert("Hello" + first + " " + last + "!");
 
 
-//for(var i=0; i<501; i=i+100) {
-// console.log(i);
-//}
-//
-//
-//for(var i=1; i<65; i=i*2) {
-// console.log(i);
-//}
-//
-//
-//for(var i=1; i<2; i=i+1) {
-// console.log(i);
-//}
-//	for(var i=1; i<2; i=i+1) {
- //console.log(i);
-//}
-//		for(var i=1; i<2; i=i+1) {
-// console.log(i);
-//}
-//for(var i=2; i<3; i=i+1) {
-// console.log(i);
-//}
-//	for(var i=2; i<3; i=i+1) {
-// console.log(i);
-//}
-//		for(var i=2; i<3; i=i+1) {
-// console.log(i);
-//}
-//for(var i=3; i<4; i=i+1) {
-// console.log(i);
-//}
-//	for(var i=3; i<4; i=i+1) {
-// console.log(i);
-//}	
-//		for(var i=3; i<4; i=i+1) {
-// console.log(i);
-//}
-//
-//
-//for(var i=9; i>-1; i=i-1) {
-// console.log(i);
-//}
-//
-//
-//for(var i=0; i<11; i=i+2) {
- //console.log(i);
-//}
-//
-//
-//for(var i=3; i<16; i=i+3) {
- //console.log(i);
-//}
-//
-//
-//for(var i=9; i>-1; i=i-1) {
-// console.log(i);
-//}
-//
-//
+for(var i=0; i<501; i=i+100) {
+console.log(i);
+}
+
+
+for(var i=1; i<65; i=i*2) {
+console.log(i);
+}
+
+
+for(var i=1; i<2; i=i+1) {
+console.log(i);
+}
+	for(var i=1; i<2; i=i+1) {
+ console.log(i);
+}
+		for(var i=1; i<2; i=i+1) {
+console.log(i);
+}
+for(var i=2; i<3; i=i+1) {
+console.log(i);
+}
+	for(var i=2; i<3; i=i+1) {
+console.log(i);
+}
+		for(var i=2; i<3; i=i+1) {
+console.log(i);
+}
+for(var i=3; i<4; i=i+1) {
+console.log(i);
+}
+	for(var i=3; i<4; i=i+1) {
+console.log(i);
+}	
+		for(var i=3; i<4; i=i+1) {
+console.log(i);
+}
+
+
+for(var i=9; i>-1; i=i-1) {
+console.log(i);
+}
+
+
+for(var i=0; i<11; i=i+2) {
+ console.log(i);
+}
+
+
+for(var i=3; i<16; i=i+3) {
+ console.log(i);
+}
+
+
+for(var i=9; i>-1; i=i-1) {
+console.log(i);
+}
+
+
 for(var i=0; i<3; i=i+1) {
  	for(var j=0; j<4; j=j+1) {
  console.log(j);
